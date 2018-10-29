9e36eb6c49e76ce2ba21f9faf8b605bec7e51afa
Edson Mattos (Lumis) <emattos@lumis.com.br>
2018-10-29 15:10:34 -0300

    REST integrado com CRUD

--- src/main/java/br/com/ebix/desafio/service/DesafioRestService.java
+++ src/main/java/br/com/ebix/desafio/service/DesafioRestService.java
@@ -1,9 +1,11 @@
 package br.com.ebix.desafio.service;
 
-import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
+
+import org.springframework.web.bind.annotation.RestController;
 
 import br.com.correios.bsb.sigep.master.bean.cliente.AtendeCliente;
 import br.com.correios.bsb.sigep.master.bean.cliente.AtendeClienteService;
@@ -11,20 +13,26 @@ import br.com.correios.bsb.sigep.master.bean.cliente.EnderecoERP;
 import br.com.correios.bsb.sigep.master.bean.cliente.SQLException_Exception;
 import br.com.correios.bsb.sigep.master.bean.cliente.SigepClienteException;
 
-@ApplicationPath("desafio")
+@RestController
 @Path("/correios")
 public class DesafioRestService {
 	
 	@GET
-	@Path("/cep")
-	public Response retornaCep() {
+	@Path("/cep/{cep}")
+	public Response retornaCep(@PathParam("cep") String cep) {
 		  AtendeClienteService atendeClienteService = new AtendeClienteService();
 		  AtendeCliente atendeCliente = atendeClienteService.getAtendeClientePort();
 		  String retorno = "";
 		  
 		  try {
-			  EnderecoERP enderecoERP = atendeCliente.consultaCEP("24120350"); 
-		  	  retorno = enderecoERP.getEnd();
+			  EnderecoERP enderecoERP = atendeCliente.consultaCEP(cep);
+			  StringBuilder strBuilder = new StringBuilder(enderecoERP.getEnd())
+					  .append(enderecoERP.getComplemento()).append(" ")
+					  .append(enderecoERP.getComplemento2()).append(" ")
+					  .append(enderecoERP.getBairro()).append(" ")
+					  .append(enderecoERP.getCidade()).append(" ")
+					  .append(enderecoERP.getUf());
+		  	  retorno = strBuilder.toString();
 		  } catch (SigepClienteException sce) {
 			  return Response.serverError().status(502, sce.getMessage()).build();
 		  } catch (SQLException_Exception see) {
